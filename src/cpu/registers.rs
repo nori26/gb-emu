50a081f67b4039f250b7fa6f62600ea6492c8279
nori <nori623p@gmail.com>
2025-05-21 23:01:50 +0900

    Reg16型を追加 レジスタに書き込み可能にした

--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -1,3 +1,5 @@
+use std::cell::RefCell;
+
 #[derive(Default, Debug)]
 pub struct Registers {
     pc: u16,
@@ -6,5 +8,20 @@ pub struct Registers {
 impl Registers {
     pub fn pc(&self) -> u16 {
         self.pc
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct Reg16 {
+    value: RefCell<u16>,
+}
+
+impl Reg16 {
+    pub fn read(&self) -> u16 {
+        *self.value.borrow()
+    }
+
+    pub fn write(&self, val: u16) {
+        *self.value.borrow_mut() = val;
     }
 }
