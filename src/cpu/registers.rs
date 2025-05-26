327a18cda3aed2d3cb73bebdc2ad5cf39842945b
nori <nori623p@gmail.com>
2025-05-27 03:35:41 +0900

    registerにoperands::Register トレイトを実装

--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -3,3 +3,21 @@ pub mod gpr;
 pub mod pc;
 
 pub use pc::ProgramCounter;
+
+use crate::cpu;
+use crate::cpu::operands;
+
+impl<REG> operands::Register for REG
+where
+    REG: cpu::Register,
+{
+    type Value = REG::Value;
+
+    fn read(&self) -> Self::Value {
+        self.load()
+    }
+
+    fn write(&self, val: Self::Value) {
+        self.store(val);
+    }
+}
